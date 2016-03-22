bd9fd1f34b2b66284615ba8c896d89ef853b115d
john97 <johndecent06@gmail.com>
2016-03-22 18:36:04 +0530

    Added files via upload

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+     int i,l;
+     cin>>i;
+     cout<<i;
+     l=strlen("keshav");
+     cout<<l;
+//	scanf("%d",&i);
+//	printf("%d",i);
+	return 0;
+}
